2579520b714e1e73942034afce0b32087d2fae75
Martin Harris <1488244+nakomis@users.noreply.github.com>
2026-04-09 15:46:43 +0100

    shepherd: drone generation (round 1)

---
+++ infra/lambda/api/ping.ts
@@ -0,0 +1,11 @@
+import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
+
+export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ pong: true }),
+  };
+};
